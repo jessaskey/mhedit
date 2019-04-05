331bb9c8a337660cb32de8f56b558a5dced14872
Jess Askey <jessaskey@users.noreply.github.com>
2019-04-05 08:32:59 -0600

    -Added ExceptionSummary.cs for basic shipping container of Exception data to web service, this will supported 'InnerExceptions' and is also serializable.

---
+++ mhedit.Containers/ExceptionSummary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mhedit.Containers
+{
+    public class ExceptionSummary
+    {
+        public String Message { get; set; }
+        public String Source { get; set; }
+        public String StackTrace { get; set; }
+        public ExceptionSummary InnerException { get; set; }
+
+        public String Username { get; set; }
+        public String ProgramVersion { get; set; }
+    }
+}
