ed7e413874cdbd5dd2af1839aa97b148f98aea27
bakerhillpins <bakerhillpins@gmal.com>
2022-07-05 13:52:48 -0400

    Fix: Missing using causing build problem.

--- MajorHavocEditor/Form2.cs
+++ MajorHavocEditor/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Windows.Forms;
 using Krypton.Docking;
 using Krypton.Toolkit;
+using mhedit.Containers;
 using MajorHavocEditor.Controls.Commands;
 using MajorHavocEditor.Controls.Menu;
 using MajorHavocEditor.Interfaces.Ui;
