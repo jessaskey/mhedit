9a140d31b38b9b7c64103d393466d574d0b6adf0
BryanRoth <bryan.roth@hypertherm.com>
2022-08-18 17:02:35 -0400

    Fix: Undo the Token Serialization page 6 bump
    Jess moved Token data before the dynamic base so this is no longer needed.

--- mhedit.GameControllers/MajorHavocPromisedEnd.cs
+++ mhedit.GameControllers/MajorHavocPromisedEnd.cs
@@ -1460,7 +1460,7 @@ namespace mhedit.GameControllers
             {
                 allTokenBytes.AddRange(encoding.Bytes);
             }
-            currentAddressPage6 += WritePagedROM( _exports[ "mtok" ], allTokenBytes.ToArray(), 0, 6 );
+            WritePagedROM(_exports["mtok"], allTokenBytes.ToArray(), 0, 6); 
 
             //****************
             //Oxoid data
